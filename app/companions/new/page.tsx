31c9958868a2b8d99e357a1c0e77a975d984c40f
Shubh Barnwal <banrwalshubh8002434@gmail.com>
2025-07-20 10:39:59 +0530

    Implemented Auth, Billing, Database Setup, Create Companion Server action

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,7 +1,16 @@
-
-const NewCampanion = () => {
+import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+const NewCampanion = async () => {
+  const { userId } = await auth();
+  if (!userId) redirect('/sign-in');
   return (
-    <div>NewCampanion</div>
+    <main className="min-lg:w-1/3 min md:w-2/3 items-center justify=center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main>
   )
 }
 
